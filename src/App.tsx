33bf9ce18bd937220332d44647c75bb93805e826
Omri Ben Ari <omri79@gmail.com>
2023-07-19 19:18:36 +0300

    using axios and react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
 import Layout from './pages/Layout';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
 import About from './pages/About';
 
+// Create a client
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="*" element={<Navigate to="/" replace={true} />} />
-      </Route>
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Route>
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
